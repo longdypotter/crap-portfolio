9b4dc38016e2aec99dd8286bc09d3d54c362c8d3
iamlongdy <longdy.potter@gmail.com>
2018-12-19 12:47:19 +0700

    added src update awesome

--- src/answersome.js
+++ src/answersome.js
@@ -1,6 +1,6 @@
 export default{
     sauce:function(){
         alert('your crap website is now been awesome sauced');
-        console.log('you');
+        console.log('you crap website website is now awesome');
     }
 }
